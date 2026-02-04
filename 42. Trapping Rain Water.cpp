ad23133a0f9409159fa4adc0d58dcbea4cb817b4
Aniket kumar singh <aniket70045@gmail.com>
2026-02-04 10:27:39 +0530

    Question no 42

---
+++ 42. Trapping Rain Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int leftmax = 0, rightmax = 0, water = 0;
+        
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                leftmax = max(leftmax, height[left]);
+                water += leftmax - height[left];
+                left++;
+            } else {
+                rightmax = max(rightmax, height[right]);
+                water += rightmax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+};
