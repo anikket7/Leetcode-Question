2d4a6a5552cbb1c07d3fa06e12157382b80a7cbd
Aniket kumar singh <aniket70045@gmail.com>
2026-02-11 20:16:48 +0530

    Question no 1413

---
+++ 1413. Minimum Value to Get Positive Step by Step Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int sum = 0;
+        int minsum = 0;
+        for (int x : nums) {
+            sum += x;
+            minsum = min(sum, minsum);
+        }
+        return 1 - minsum;
+    }
+};
