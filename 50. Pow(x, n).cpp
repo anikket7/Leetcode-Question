d6cd2a9d64335c1108320ba39c054af6a4eb3185
Aniket kumar singh <aniket70045@gmail.com>
2026-02-19 10:04:44 +0530

    Question no 50

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        long long pow = n;
+        if (pow < 0) {
+            x = 1 / x;
+            pow = -pow;
+        }
+        while (pow > 0) {
+            if (pow % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            pow /= 2;
+        }
+        return ans;
+    }
+};
